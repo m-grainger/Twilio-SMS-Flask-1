bbe96a2200a459eeef2d62e75ee4e5141e76e7a8
Matt Grainger <matt.grainger@smarsh.com>
2021-07-14 11:52:55 -0700

    broke out responses into three functions

--- app/main.py
+++ app/main.py
@@ -6,20 +6,23 @@ app = Flask(__name__)
 
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
+def sms_reply1():
     """Respond to incoming calls with a MMS message."""
     # Start our TwiML response
     resp = MessagingResponse()
-
     # Add a text message
     msg = resp.message("The Robots are coming! Head for the hills!")
-    time.sleep(2)
-    msg = resp.message("... it's too late- they got me!")
-    time.sleep(10)
+    return str(resp)
 
+def sms_reply2():
+    resp=MessagingResponse()
+    msg = resp.message("... it's too late- they got me!")
+    return str(resp)
+
+def sms_reply3():
+    resp=MessagingResponse()
     # Add a picture message
     msg.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
-
     return str(resp)
 
 if __name__ == "__main__":
