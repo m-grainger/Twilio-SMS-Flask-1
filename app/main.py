3e8477c09b53c81e10bd5ec06050b443f9163072
Matt Grainger <matt.grainger@smarsh.com>
2021-07-14 12:01:04 -0700

    re-pasted

--- app/main.py
+++ app/main.py
@@ -1,28 +1,21 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-import time
 
 app = Flask(__name__)
 
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_reply1():
+def sms_reply():
     """Respond to incoming calls with a MMS message."""
     # Start our TwiML response
     resp = MessagingResponse()
+
     # Add a text message
     msg = resp.message("The Robots are coming! Head for the hills!")
-    return str(resp)
 
-def sms_reply2():
-    resp=MessagingResponse()
-    msg = resp.message("... it's too late- they got me!")
-    return str(resp)
-
-def sms_reply3():
-    resp=MessagingResponse()
     # Add a picture message
     msg.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
+
     return str(resp)
 
 if __name__ == "__main__":
