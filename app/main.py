04d648d83ba24d7a756b1c47db05033b070c1041
Matt Grainger <matt.grainger@smarsh.com>
2021-07-14 11:08:18 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ def sms_reply():
 
     # Add a text message
     msg = resp.message("The Robots are coming! Head for the hills!")
+    msg = resp.message("... it's too late- they got me!")
 
     # Add a picture message
     msg.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
