84b8bdaece8a5a97bd202f6a1f3f5e91a0706c5e
Matt Grainger <matt.grainger@smarsh.com>
2021-07-15 14:11:19 -0700

    removed custom sms route

--- app/main.py
+++ app/main.py
@@ -24,21 +24,6 @@ def incoming_sms():
     message_status = request.values.get('MessageStatus', None)
     logging.info('SID: {}, Status: {}'.format(message_sid, message_status))
     return ('', 204)
-    
-@app.route("/sms-custom", methods=['GET', 'POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
-    # Start our TwiML response
-    resp = MessagingResponse()
-    # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
-
-    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
